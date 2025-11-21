1a2ed4043fa79791fe3eb5bf50e80cd045eb5767
vuleminh2002 <vuleminh2016@gmail.com>
2025-11-21 15:34:06 -0500

    wer

--- finetune2/test2.py
+++ finetune2/test2.py
@@ -14,7 +14,7 @@ from peft import PeftModel
 # ============================================================
 
 BASE_MODEL = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-LORA_DIR   = "tinyllama-geocode-lora_s1"
+LORA_DIR   = "tinyllama-geocode-lora_s2"
 TEST_FILE  = "geocode_train_vary_test.jsonl"
 
 MAX_NEW_TOKENS = 512
